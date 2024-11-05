4a73d136f43d73c06653795d641861510d4fd9c9
Kunori47 <yuualph47@gmail.com>
2024-11-04 23:31:23 -0400

    forgot password

---
+++ src/pages/api/auth/forgot.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    const formData = await request.formData();
+    const email = formData.get("email")?.toString();
+    const password = formData.get("password")?.toString();
+  
+    if (!email || !password) {
+      return new Response("Email and password are required", { status: 400 });
+    }
+  
+    
+    const { data, error } = await supabase.auth.updateUser({
+        email,
+        password: password,
+        data: { hello: 'world' }
+      })
+
+    if (error) {
+      return new Response(error.message, { status: 500 });
+    }
+  
+    return redirect("/signin");
+    
+  };
+
+  export const GET: APIRoute = async ({redirect}) => {
+    // You can return a simple message or render a page for the forgot password
+    return redirect("/signin");
+};
